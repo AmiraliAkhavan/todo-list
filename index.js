d220e59c1891214e59425f57c23f38d11e4e1904
AmiraliAkhavan <111018923+AmiraliAkhavan@users.noreply.github.com>
2023-03-11 03:14:37 +0330

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const dotenv = require("dotenv").config();
+// const dotenv = require("dotenv").config();
 const mongoose = require("mongoose");
 const cors = require("cors");
 const app = express();
@@ -13,11 +13,12 @@ app.use(cors());
 
 const TodoItemRoute = require("./routes/routeItems");
 
-mongoose
-  .connect(process.env.DB_CONNECT)
-  .then(() => console.log("Database connected"))
-  .catch((err) => console.log(err));
-
 app.use("/", TodoItemRoute);
 
-app.listen(PORT, () => console.log("Server connected"));
+mongoose
+  .connect(process.env.DB_CONNECT)
+  .then(() => {
+    console.log("Database connected");
+    app.listen(PORT, () => console.log("Server connected"));
+  })
+  .catch((err) => console.log(err));
