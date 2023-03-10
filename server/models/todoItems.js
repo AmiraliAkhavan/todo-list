170bf45878bf1e4e0bcd2b416d2cda12e7423da7
AmiraliAkhavan <111018923+AmiraliAkhavan@users.noreply.github.com>
2023-03-11 02:54:55 +0330

    front-end

--- server/models/todoItems.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-const todoItemSchema = new mongoose.Schema({
-  item: {
-    type: String,
-    required: true,
-  },
-});
-
-module.exports = mongoose.model("todo", todoItemSchema);
